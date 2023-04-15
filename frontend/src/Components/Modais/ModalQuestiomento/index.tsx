9d6bbc216108cf3ff0b9667d669c212309e67fdf
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2023-04-15 13:07:46 -0300

    Frontent: adição: Adição de Modal questionamento e Deletar

--- frontend/src/Components/Modais/ModalQuestiomento/index.tsx
+++ frontend/src/Components/Modais/ModalQuestiomento/index.tsx
@@ -1,21 +1,22 @@
 import React from "react";
-import { ModalStyle, ModalBackgroundStyle } from "./styles";
+
+import * as Modal from "./styles";
+import { BsExclamationCircleFill } from "react-icons/bs";
 
 type Props = {
-    children?: JSX.Element[] | JSX.Element;
-  }
-  
-
-const ModalQuestiomento :React.FC<Props> = ({
-    children,
-}) => (
-        <ModalBackgroundStyle>
-            <ModalStyle>
-                <img src="/assets/question.svg" alt="question"></img>
-                <h4>Tem certeza que quer realizar a operacão</h4>
-                { children }
-            </ModalStyle>
-        </ModalBackgroundStyle>
-);
-
-export default ModalQuestiomento;
+  children?: any;
+};
+export const QuestionamentoModal: React.FC<Props> = ({ children }) => {
+  return (
+    <Modal.BackGround>
+      <Modal.Body>
+        <BsExclamationCircleFill
+          size={"120px"}
+          color="#f58619"
+        ></BsExclamationCircleFill>
+        <h3>Tem certeza que deseja realizar essa operação!</h3>
+        {children}
+      </Modal.Body>
+    </Modal.BackGround>
+  );
+};
