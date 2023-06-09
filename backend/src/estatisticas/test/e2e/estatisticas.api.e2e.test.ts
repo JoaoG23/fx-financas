13f0568026ad666984cb5d2e05aaae18002beace
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2023-06-08 21:37:48 -0300

    Backend: Criação de end2end estatisticas/gasto_dozes_meses para retorna quantidade de despesa gasta para cada mes do ano requirido

--- backend/src/estatisticas/test/e2e/estatisticas.api.e2e.test.ts
+++ backend/src/estatisticas/test/e2e/estatisticas.api.e2e.test.ts
@@ -13,9 +13,9 @@ describe("ROTAS de manipulação /estatisticas", () => {
     await limparTabelaFluxoCaixa();
   });
 
-  // buscarSomaGanhosMesUsuarioSubtests()
-  // buscarSomaGastosMesUsuarioSubtests()
-  // buscarSaldoAtualUsuarioSubtests();
+  buscarSomaGanhosMesUsuarioSubtests()
+  buscarSomaGastosMesUsuarioSubtests()
+  buscarSaldoAtualUsuarioSubtests();
   buscarDespesas12MesesSubtests()
   
 });
