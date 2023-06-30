a21db1295b8bfefeea45af61f91ee4cb1b0972a7
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2023-06-30 16:55:01 -0300

    Frontend: bug não aparecer os nome das categorias na tela item fluxo de caixa

--- frontend-vite/src/Pages/Agenda/configs/eventoStyle.ts
+++ frontend-vite/src/Pages/Agenda/configs/eventoStyle.ts
@@ -2,7 +2,7 @@ export const eventoStyle = () => {
     return {
       style: {
         borderColor:'#f8f8f9',
-        backgroundColor: '#FFA26B',
+        backgroundColor: '#5A69DD',
         color: 'white'
       }
     }
