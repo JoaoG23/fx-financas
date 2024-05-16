b31a057e74b2303b8db0d7a41f278295d6f0f86a
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2024-05-16 20:46:28 -0300

    correcao de graficos e imagem perfil dos usuários (#2)

--- frontend-vite/src/Components/Navs/Header/index.tsx
+++ frontend-vite/src/Components/Navs/Header/index.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 
 import { FaUser } from "react-icons/fa";
 import { BsFillArrowRightCircleFill } from "react-icons/bs";
 
-import { Container, ImageContainer, VoltarText, TextLimited, VoltarContainer } from "./styles";
+import {
+  Container,
+  ImageContainer,
+  VoltarText,
+  TextLimited,
+  VoltarContainer,
+} from "./styles";
 import { buscaDadoUsuarioNaSessao } from "../../../utils/buscaDadoUsuarioNaSessao";
 import { ImagemPerfil } from "./components/ImagePerfil";
 
 const Header: React.FC = () => {
   const { nomeUsuario, imagePerfil } = buscaDadoUsuarioNaSessao();
+  const [perfil, serPerfil] = useState<string>(imagePerfil || "");
 
   const navigate = useNavigate();
   function voltarPaginaAnterior() {
@@ -19,7 +26,7 @@ const Header: React.FC = () => {
     <Container>
       <Link to={"/usuario_logado"}>
         <ImageContainer>
-          <ImagemPerfil caminho_imagem={imagePerfil!} />
+          <ImagemPerfil caminho_imagem={perfil} />
           <TextLimited>{nomeUsuario}</TextLimited>
         </ImageContainer>
       </Link>
