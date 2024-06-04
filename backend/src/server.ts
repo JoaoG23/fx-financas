89deab3c9226c06854f69e39658f69294809af8a
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2024-06-04 15:10:24 -0300

    :zap: perf: Adicionado encerramento adequado ao finalizar aplicação

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,15 @@
 import app from "./app";
+import { PrismaConexao } from "./configs/PrismaConexao";
 
+const prisma = PrismaConexao.getInstancia();
 const port = process.env.PORT_SERVER || 3000;
 
 app.listen(port, async () => {
   console.info(`API Fx-finances rodando na porta : ${process.env.PORT_SERVER}`);
+});
+
+process.on("SIGINT", async () => {
+  await prisma.$disconnect();
+  console.log("Shutdown complete.");
+  process.exit(0);
 });
