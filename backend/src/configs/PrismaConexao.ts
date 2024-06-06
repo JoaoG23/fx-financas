fecd58831f692f73a7ce8d7ddf162a15ce0ae932
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2024-06-06 18:20:55 -0300

    :zap: perf: adição de monitoramento de log conexao com banco

--- backend/src/configs/PrismaConexao.ts
+++ backend/src/configs/PrismaConexao.ts
@@ -1,10 +1,11 @@
 import { PrismaClient } from "@prisma/client";
-
 export class PrismaConexao extends PrismaClient {
   private static prismaClient: PrismaClient | null = null;
   public static getInstancia(): PrismaConexao {
     if (PrismaConexao.prismaClient === null) {
-      return (PrismaConexao.prismaClient = new PrismaConexao());
+      return (PrismaConexao.prismaClient = new PrismaConexao({
+        log: ["info"],
+      }));
     }
     return PrismaConexao.prismaClient;
   }
